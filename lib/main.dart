74cdd35991b4aac001fae981473612a1fc1e1776
FlutLab.io <>
2022-05-02 14:12:11 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Centro de computo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> images = [
+    "assets/images/gh.jpg",
+    "assets/images/oh.jpg",
+    "assets/images/oooo.jpg",
+    "assets/images/op.jpg",
+    "assets/images/pe.jpg",
+    "assets/images/uh.jpg",
+    "assets/images/ta.jpg",
+    "assets/images/a.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter GridView"),
+        ),
+        body: GridView.custom(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 8,
+          ),
+          padding: const EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
